9987c9340a98d12a81e212ce1612f4442f4c4040
Eino-Ville Talvala <etalvala@google.com>
2015-04-07 14:29:00 -0700

    Camera: Add module init placeholder
    Change-Id: I6d0e4bde4e8cd26db26268fe7be43094c71a71db

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -65,6 +65,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
     .get_vendor_tag_ops = NULL, /* remove compilation warnings */
     .open_legacy = NULL, /* remove compilation warnings */
     .set_torch_mode = NULL, /* remove compilation warnings */
+    .init = NULL, /* remove compilation warnings */
     .reserved = {0}, /* remove compilation warnings */
 };
 
