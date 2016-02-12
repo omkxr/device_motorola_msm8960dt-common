4def3233eb8ea7fcedc4bf5cb43114e0f165de01
Ethan Chen <intervigil@gmail.com>
2016-02-11 22:58:05 -0800

    msm8960dt: Fix mount_pds script for toybox
    Change-Id: I5920c53de18260d743baa6205b5d7cdd3b8386a1

--- rootdir/etc/mount_pds.sh
+++ rootdir/etc/mount_pds.sh
@@ -4,12 +4,12 @@ export PATH=/system/xbin:$PATH
 if [ ! -f /cache/pds-CM10.img ]
 then
     #make a copy of pds in /cache
-    /system/xbin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=/cache/pds-CM10.img
+    /system/bin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=/cache/pds-CM10.img
     echo "Backed up PDS"
 fi
 
 #mount the fake pds
-/system/xbin/losetup /dev/block/loop0 /cache/pds-CM10.img
-/system/xbin/busybox mount -o rw /dev/block/loop0 /pds
+/system/bin/losetup /dev/block/loop0 /cache/pds-CM10.img
+/system/bin/mount -o rw /dev/block/loop0 /pds
 /system/bin/restorecon -R /pds
 echo "Mounted PDS"
