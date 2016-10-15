edeb20ae5f8841c759dda79ff24d59f8dbc597ef
Omkar <djomi400@gmail.com>
2016-10-15 12:12:36 +0000

    Revert "msm8960dt-common: add android fs config"
    This reverts commit eb04932a2e913763583b1ada6ec162f3adf1d94e.

--- msm8960dt-common.mk
+++ msm8960dt-common.mk
@@ -79,10 +79,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
 
-# Android_filesystem_config.h
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8960 \
